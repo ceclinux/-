6705b6c20c7ed2d6cab8f9e9720d0b0a9623b02c
Gitea <gitea@fake.local>
2020-07-15 21:04:00 +0100

    module attributs

--- Learn Elixir/math.ex
+++ Learn Elixir/math.ex
@@ -1,6 +1,17 @@
 defmodule Math do
-  def sum(a, b) do
-    a + b
-  end
+  @moduledoc """
+  Provides math-related functions.
+
+  ## Examples
+
+      iex> Math.sum(1, 2)
+      3
+
+  """
+
+  @doc """
+  Calculates the sum of two numbers.
+  """
+  def sum(a, b), do: a + b
 end
 
